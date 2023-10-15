9f179b74ee8fee613c7a529951fc5d8d70785eac
prayatnaaa <tudeprayatna04@gmail.com>
2023-10-15 21:28:34 +0800

    removing unnecesarry coments

--- controllers/apotekControllers.js
+++ controllers/apotekControllers.js
@@ -1,30 +1,18 @@
 const Apotek = require("../models/Apotek");
 
-// Dibawah ini kita menggunakan metod export, maka semua metod yang ada di dalam object(module.exports) akan ter export
 module.exports = {
-  // Membuat view untuk mahasiswa
   viewApotek: async (req, res) => {
     try {
-      // Membuat variabel mahasiswa, dan menunda eksekusi hingga proses async selesai lalu mengambil model Mahasiswa
-      // dan menggunakan method find untuk mengambil semua collection/tabel yang ada di database Mahasiswa
       const apotek = await Apotek.find();
-      // Membuat variabel untuk alertMessage  dan alertStatus
       const alertMessage = req.flash("alertMessage");
       const alertStatus = req.flash("alertStatus");
-      // membuat variabel yang bersifat object dan memiliki sebuah pesan isinya mengambil dari variabel alertMessage dan alertStatus
       const alert = { message: alertMessage, status: alertStatus };
-      /**
-       * Lalu render viewnya yang ada di dalam file index
-       * menampilkan datanya dan mendestracturkan nya, lalu memanggil variabel mahasiswa diatas
-       * Lalu merender alert yang sudah di deklar di atas
-       */
       res.render("index", {
         apotek,
         alert,
         title: "OBAT", // Untuk title dari aplikasi kita, saya manamakannya dengan CRUD
       });
     } catch (error) {
-      // Jika error maka akan meredirect ke route mahasiswa(routenya akan kita buat setelah selesai dengan mahasiswaController)
       res.redirect("/apotek");
     }
   },
